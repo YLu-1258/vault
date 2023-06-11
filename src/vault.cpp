4a0861b1ba9924417267f88d5a199cdc99e7898a
Ylu-1258 <maodou1258@gmail.com>
2023-06-10 19:51:19 -0700

    open_database

--- src/vault.cpp
+++ src/vault.cpp
@@ -1,19 +1,27 @@
-#include <stdio.h>
-#include <sqlite3.h> 
+#include <string> 
+#include <iostream>
+#include <sqlite3.h>
+#include <project_path.h>
+
+
+sqlite3* open_database(const char* db_name) {
+    sqlite3 *db;
+    int rc = sqlite3_open(db_name, &db);
+    
+    if (rc) {
+        std::cout<<"Can't open database:"<<sqlite3_errmsg(db)<<std::endl;
+    } else {
+        std::cout<<"Database opened succesfully at "<<db_name<<std::endl;
+    }
+    return db;
+}
+
+int add_entry(sqlite3 *db) {
+    
+}
 
 
 int main(int argc, char* argv[]) {
-   sqlite3 *db;
-   char *zErrMsg = 0;
-   int rc;
-
-   rc = sqlite3_open("test.db", &db);
-
-   if( rc ) {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      return(0);
-   } else {
-      fprintf(stderr, "Opened database successfully\n");
-   }
-   sqlite3_close(db);
+    sqlite3 *db = open_database(DB_PATH);
+    return 0;
 }
