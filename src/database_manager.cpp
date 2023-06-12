c006bbaf7b2a524bb3defae86abe7043e6108ffd
Ylu-1258 <maodou1258@gmail.com>
2023-06-12 13:01:18 -0700

    updated database functions

--- src/database_manager.cpp
+++ src/database_manager.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <ctime>
 #include "database_manager.hpp"
 
 DatabaseManager::DatabaseManager(const char* db_name) {
@@ -23,9 +24,9 @@ DatabaseManager::~DatabaseManager() {
     }
 }
 
-bool DatabaseManager::error_handler(int rc){
+bool DatabaseManager::error_handler(int rc, char* error_message){
     if (rc) {
-        std::cout<<"Cannot execute operation, "<<sqlite3_errmsg(db)<<std::endl;
+        std::cout<<"Cannot execute operation, "<<error_message<<std::endl;
         return false;
     } else {
         std::cout<<"Operation successful"<<std::endl;
@@ -34,21 +35,29 @@ bool DatabaseManager::error_handler(int rc){
 }
 
 bool DatabaseManager::init_userpw_table(){
-    std::string query = "CREATE TABLE user_pw (entry_id INT NOT NULL AUTO_INCREMENT, service_name text NOT NULL, username text NOT NULL, user_hash text NOT NULL, created_at int, expiration_date int, updated_at int)";
-    int rc = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
-    return error_handler(rc);
+    std::string query = "CREATE TABLE user_records (entry_id INT NOT NULL AUTO_INCREMENT, service_name text NOT NULL, username text NOT NULL, user_hash text NOT NULL, created_at int, expiration_date int, updated_at int)";
+    char* error_message;
+    int rc = sqlite3_exec(db, query.c_str(), nullptr, nullptr, &error_message);
+    return error_handler(rc, error_message);
 }
 
-bool DatabaseManager::insert_entry(std::string service, std::string username, std::string hash, int created_at, int expiration_date, int updated_at) {
-    std::string query = "INSERT INTO user_pw (service_name, username, user_hash, created_at, expiration_date, updated_at) VALUES (" + service + ", " + username + ", " + hash + ");";
-    int rc = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
-    return error_handler(rc);
+bool DatabaseManager::insert_entry(const std::string& table_name, const std::string& service, const std::string& username, const std::string& hash, std::time_t created_at, std::time_t expiration_date, std::time_t updated_at) {
+    std::string query = "INSERT INTO user_records (service_name, username, user_hash, created_at, expiration_date, updated_at) VALUES (" + service + ", " + username + ", " + hash + ");";
+    char* error_message;
+    int rc = sqlite3_exec(db, query.c_str(), nullptr, nullptr, &error_message);
+    return error_handler(rc, error_message);
 }
 
-bool DatabaseManager::delete_entry(int id) {
-
+bool DatabaseManager::delete_entry(const std::string& table_name, const std::string& condition, const std::string& condition_column) {
+    std::string query = "DELETE FROM " + table_name + "WHERE " + condition_column + " = " + condition + ";";
+    char* error_message;
+    int rc = sqlite3_exec(db, query.c_str(), nullptr, nullptr, &error_message);
+    return error_handler(rc, error_message);
 }
 
-bool DatabaseManager::update_entry(int id) {
-    
+bool DatabaseManager::update_entry(const std::string& table_name, const std::string& condition_column, const std::string& condition, const std::string& column_name, const std::string& new_value) {
+    std::string query = "UPDATE " + table_name + " SET " + column_name + " = " + new_value + " WHERE " + condition_column + " = " + condition + ";";
+    char* error_message;
+    int rc = sqlite3_exec(db, query.c_str(), nullptr, nullptr, &error_message);
+    return error_handler(rc, error_message);
 }
--- src/database_manager.hpp
+++ src/database_manager.hpp
@@ -2,16 +2,19 @@
 #define  DATABASE_MANAGER_HPP
 #include <sqlite3.h>
 #include <string>
+#include <ctime>
 
 class DatabaseManager {
 public:
     DatabaseManager(const char* filename);
     ~DatabaseManager();
-    bool error_handler(int rc);
+    bool error_handler(int rc, char* error_message);
+    bool check_table_existence(const std::string& table_name);
     bool init_userpw_table();
-    bool insert_entry(std::string service, std::string username, std::string hash, int created_at, int expiration_date, int updated_at);
-    bool delete_entry(int id);
-    bool update_entry(int id);
+    bool db_access_auth(const std::string& master_passwd);
+    bool insert_entry(const std::string& table_name, const std::string& service, const std::string& username, const std::string& hash, std::time_t created_at, std::time_t expiration_date, std::time_t updated_at);
+    bool delete_entry(const std::string& table_name, const std::string& condition, const std::string& condition_column);
+    bool update_entry(const std::string& table_name, const std::string& condition_column, const std::string& condition, const std::string& column_name, const std::string& new_value);
 
 private:
     sqlite3* db;
