367c95d064b603a2e782d51e3e7b99790e81be4e
Ylu-1258 <maodou1258@gmail.com>
2023-06-20 10:31:45 -0700

    shift rows + sub_bytes

--- src/AES.cpp
+++ src/AES.cpp
@@ -1,10 +1,64 @@
 #include "AES.h"
 #include <iostream>
 
+
 unsigned char* AES::generate_AES_Key() {
     unsigned char* key = new unsigned char[KEY_LENGTH];         // Specify character array for key
     if (RAND_bytes(key, KEY_LENGTH) != 1) {                     // Try to generate key
         throw std::runtime_error("Failed to generate AES key"); // Key generation failed
     }
     return key;
+}
+
+void AES::sub_bytes(){
+    unsigned char row, column;
+    for (int i = 0; i < STATE_SIZE; i++) {
+        for (int j = 0; j < STATE_SIZE; j++) {
+            std::tie(row, column) = extract_nibble(&state[i][j]);
+            state[i][j] = s_box[row][column];
+        }
+    }
+}
+
+void AES::inv_sub_bytes(){
+    unsigned char row, column;
+    for (int i = 0; i < STATE_SIZE; i++) {
+        for (int j = 0; j < STATE_SIZE; j++) {
+            std::tie(row, column) = extract_nibble(&state[i][j]);
+            state[i][j] = inv_s_box[row][column];
+        }
+    }
+}
+
+void AES::rot_word_left(int row, int rotation){
+    // we want to calculate new indexes
+    byte word[STATE_SIZE];
+    for (int i = 0; i < STATE_SIZE; i++) {
+        word[i] = state[row][i];
+    }
+    for (int j = 0; j < STATE_SIZE; j++) {
+        int new_index = calculate_new_rotation_index(j-rotation);
+        state[row][new_index] = word[j];
+    }
+}
+
+int AES::calculate_new_rotation_index(int idx){
+    return (idx < 0) ? 3-(abs(idx)-1) : idx;
+}
+
+void AES::shift_rows(){
+    for (int i = 0; i < STATE_SIZE; i++) {
+        rot_word_left(i, i);
+    }
+}
+
+std::tuple<byte, byte> AES::extract_nibble(byte* source) {
+    const int first_nibble = (*source & 0xF0) >> 4;
+	const int second_nibble = (*source & 0xF) >> 0;
+
+    return {first_nibble, second_nibble};
+}
+
+byte* AES::get_state() {
+    return &state[0][0];
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <string> 
+#include <sstream>
 #include <iostream>
 #include <ctime>
 #include <sqlite3.h>
@@ -6,9 +7,29 @@
 #include "database_manager.h"
 #include "AES.h"
 
+std::string print_array(unsigned char* arr) {
+    std::stringstream ss;
+    int cols = 4;
+    int rows = 4;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            ss<<std::hex<<static_cast<int>(arr[i*rows + j])<<" ";
+        } 
+        ss<<"\n";
+    }
+    return ss.str();
+}
+
 int main(int argc, char* argv[]) {
-    DatabaseManager DB_manager(DB_PATH);
+    // DatabaseManager DB_manager(DB_PATH);
+    // DB_manager.init_userpw_table();
     AES AES_manager;
-    DB_manager.init_userpw_table();
+    std::cout<<"Original State: "<< "\n" <<print_array(AES_manager.get_state())<<std::endl;
+    AES_manager.sub_bytes();
+    std::cout<<"Subbed State: "<< "\n" <<print_array(AES_manager.get_state())<<std::endl;
+    AES_manager.inv_sub_bytes();
+    std::cout<<"Inverse Subbed State: "<< "\n" <<print_array(AES_manager.get_state())<<std::endl;
+    AES_manager.shift_rows();
+    std::cout<<"Shifted Rows"<< "\n" <<print_array(AES_manager.get_state())<<std::endl;
     return 0;
 }
